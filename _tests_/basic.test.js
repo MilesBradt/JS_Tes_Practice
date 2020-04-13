35c55a7cc32c35a5ca5ae3d4ffebf7dc1e129656
MilesBradt <smbradtmichael@gmail.com>
2020-04-13 16:41:57 -0700

    move changeCounter outside function, still same error

--- _tests_/basic.test.js
+++ _tests_/basic.test.js
@@ -3,12 +3,24 @@ import { changeCounter } from '../src/main.js';
 describe('Change Counter', () => {
 
     test('should return 1 quarter from 0.25', () => {
-        var testOne = changeCounter(0.25).quarters
+        var changeCount = {
+            quarters: 0,
+            dimes: 0,
+            nickels: 0,
+            pennies: 0
+        }
+        var testOne = changeCounter(0.25, changeCount).quarters
         expect(testOne).toEqual(1);
     });
 
     test('should return 2 quarters from 0.50', () => {
-        var testTwo = changeCounter(0.50).quarters
+        var changeCount = {
+            quarters: 0,
+            dimes: 0,
+            nickels: 0,
+            pennies: 0
+        }
+        var testTwo = changeCounter(0.50, changeCount).quarters
         expect(testTwo).toEqual(2);
     });
 });
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-export function changeCounter(money) {
-    let change = {
-        quarters: 0,
-        dimes: 0,
-        nickels: 0,
-        pennies: 0
-    }
+let changeCount = {
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0
+}
+
+export function changeCounter(money, change) {
     if (money - 0.25 >= 0.25 || money - 0.25 == 0){
-        money = money - 0.25;
+        money = money - 0.25
         change.quarters = change.quarters + 1;
-        changeCounter(money)
+        changeCounter(money, change)
     }
-    if (money == 0) {
+    if (money <= 0) {
         return change
     }
 };
